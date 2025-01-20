a76565c4c67db5333a0065b67e31327a3f73b113
littleBitsman <adittyanischal@hotmail.com>
2025-01-19 20:21:25 -0500

    code again

---
+++ LostInSpaceLib.cpp
@@ -0,0 +1,150 @@
+// These are wrappers for Astrobee wee
+
+#include "math.h"
+#include "LostInSpaceLib.h"
+
+double Position::DistanceTo(Position* other) const
+{
+    return sqrt(pow(this->x - other->x, 2) + pow(this->y - other->y, 2));
+}
+double Position::DistanceTo(Position& other) const
+{
+    return DistanceTo(&other);
+}
+
+Position Debris::GetLocation()
+{
+    return GetObjectLocation(this);
+}
+Position Debris::GetPosition()
+{
+    return GetLocation();
+}
+double Debris::DistanceFromAstrobee()
+{
+    return GetPosition().DistanceTo(GetRobotPosition());
+}
+
+Position GetRobotPosition()
+{
+    return Position(game.GetRobotPositionX(), game.GetRobotPositionY());
+}
+float GetTime()
+{
+    return game.GetTime();
+}
+float GetBattery()
+{
+    return game.GetBattery();
+}
+float GetScore()
+{
+    return game.GetScore();
+}
+const Debris* GetObjectBeingCarried()
+{
+    if (game.GetObjectBeingCarried() >= 0)
+    {
+        // SAFETY (yes im pulling a Rust): 
+        // We just checked if the value is >= 0
+        return &(DebrisList[game.GetObjectBeingCarried()]);
+    }
+    else
+    {
+        return nullptr;
+    }
+}
+Position GetObjectLocation(int obj)
+{
+    return Position(game.GetObjectLocX(obj), game.GetObjectLocY(obj));
+}
+Position GetObjectLocation(Debris* debris)
+{
+    return GetObjectLocation(debris->id);
+}
+Position GetObjectLocation(Debris& debris)
+{
+    return GetObjectLocation(&debris);
+}
+bool MoveTo(float x, float y)
+{
+    return game.MoveTo(x, y);
+}
+bool MoveTo(Position* pos)
+{
+    return MoveTo(pos->x, pos->y);
+}
+bool MoveTo(Position& pos)
+{
+    return MoveTo(&pos);
+}
+bool MoveToHome()
+{
+    return game.MoveToHome();
+}
+bool GrabObject(int obj)
+{
+    return game.GrabObject(obj);
+}
+bool GrabObject(Debris* debris)
+{
+    return GrabObject(debris->id);
+}
+bool GrabObject(Debris& debris)
+{
+    return GrabObject(&debris);
+}
+bool DropObject()
+{
+    return game.DropObject();
+}
+void EndGame()
+{
+    game.EndGame();
+}
+void MoveToVoid(float x, float y)
+{
+    game.MoveToVoid(x, y);
+}
+void MoveToVoid(Position* pos)
+{
+    MoveToVoid(pos->x, pos->y);
+}
+void MoveToVoid(Position& pos)
+{
+    MoveToVoid(&pos);
+}
+void GrabObjectVoid(int obj)
+{
+    game.GrabObjectVoid(obj);
+}
+void GrabObjectVoid(Debris* debris)
+{
+    GrabObjectVoid(debris->id);
+}
+void GrabObjectVoid(Debris& debris)
+{
+    GrabObjectVoid(&debris);
+}
+Debris(*GetAllDebris())[15] {
+    return &DebrisList;
+}
+Debris* GetNearestDebris()
+{
+    int nearestId = -1;
+    double nearestDistance = 100; // No debris can be more than sqrt(2) (?) units away, this handles that pretty well
+
+    for (int i = 0; i < 15; i++)
+    {
+        Debris curr = DebrisList[i];
+        double distance = curr.DistanceFromAstrobee();
+        if (distance < nearestDistance)
+        {
+            nearestId = i;
+            nearestDistance = distance;
+        }
+    }
+
+    // SAFETY: Some debris must exist; therefore at some point nearestId != -1 will hold true permanently
+    return &DebrisList[nearestId];
+}
---
+++ LostInSpaceLib.h
@@ -0,0 +1,77 @@
+// These are wrappers for Astrobee wee
+
+#ifdef __has_include
+#if __has_include("Astrobee.h")
+#include "Astrobee.h"
+#endif
+#endif
+
+class Position
+{
+public:
+    const float x = 0.0f;
+    const float y = 0.0f;
+    // Position() : x(0.0f), y(0.0f) {}
+    Position(float x, float y) : x(x), y(y) {}
+    double DistanceTo(Position *other) const;
+    double DistanceTo(Position &other) const;
+};
+
+class Debris
+{
+public:
+    const unsigned int id;
+    const unsigned int mass;
+    Debris(unsigned int id) : id(id), mass(game.GetObjectMass(id)) {}
+    Position GetLocation();
+    // Alias for `GetLocation`
+    Position GetPosition();
+    // I love the distance formula
+    // You love the distance formula
+    // We all love the distance formula
+    double DistanceFromAstrobee();
+};
+
+Position GetRobotPosition();
+float GetTime();
+float GetBattery();
+float GetScore();
+const Debris *GetObjectBeingCarried();
+Position GetObjectLocation(int obj);
+Position GetObjectLocation(Debris *debris);
+Position GetObjectLocation(Debris &debris);
+bool MoveTo(float x, float y);
+bool MoveTo(Position *pos);
+bool MoveTo(Position &pos);
+bool MoveToHome();
+bool GrabObject(int obj);
+bool GrabObject(Debris *debris);
+bool GrabObject(Debris &debris);
+bool DropObject();
+void EndGame();
+void MoveToVoid(float x, float y);
+void MoveToVoid(Position *pos);
+void MoveToVoid(Position &pos);
+void GrabObjectVoid(int obj);
+void GrabObjectVoid(Debris *debris);
+void GrabObjectVoid(Debris &debris);
+Debris (*GetAllDebris())[15];
+Debris *GetNearestDebris();
+
+Debris DebrisList[15] = {
+    Debris(0),
+    Debris(1),
+    Debris(2),
+    Debris(3),
+    Debris(4),
+    Debris(5),
+    Debris(6),
+    Debris(7),
+    Debris(8),
+    Debris(9),
+    Debris(10),
+    Debris(11),
+    Debris(12),
+    Debris(13),
+    Debris(14)
+};
